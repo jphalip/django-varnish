d8cc9581ca89a85d9ed49e41422ac2f60fc5cdf1
Corey Oordt <coreyoordt@gmail.com>
2013-01-14 09:16:50 -0600

    Updated the setup.py and __init__ to use the standard files we use.

--- setup.py
+++ setup.py
@@ -1,13 +1,40 @@
-from distutils.core import setup
+import os
+from setuptools import setup, find_packages
+
+
+def read_file(filename):
+    """Read a file into a string"""
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except IOError:
+        return ''
+
+
+def get_readme():
+    """Return the README file contents. Supports text,rst, and markdown"""
+    for name in ('README', 'README.rst', 'README.md'):
+        if os.path.exists(name):
+            return read_file(name)
+    return ''
+
+# Use the docstring of the __init__ file to be the description
+DESC = " ".join(__import__('django-varnish').__doc__.splitlines()).strip()
 
 setup(
-    name = "django-varnish",
-    version = '0.1',
-    url = 'http://opensource.washingtontimes.com/projects/django-varnish/',
-    author = 'Justin Quick',
-    author_email= 'justquick@gmail.com',
-    long_description=open('README.rst').read(),
-    description = 'Integration between Django and the Varnish HTTP accelerator using the management port using telnet',
-    packages = ['varnishapp']
+    name="django-varnish",
+    version=__import__('django-varnish').get_version().replace(' ', '-'),
+    url='https://github.com/callowayproject/django-varnish',
+    author='Justin Quick, Calloway Project',
+    author_email='webmaster@callowayproject.com',
+    description=DESC,
+    long_description=get_readme(),
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=read_file('requirements.txt'),
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Framework :: Django',
+    ],
 )
-
