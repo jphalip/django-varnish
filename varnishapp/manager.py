25d6934c23252f839acb7dc4e8e99a480166699b
Jannis Leidel <jannis@leidel.info>
2013-01-24 16:49:52 +0100

    Fixed secret handling and moved signal connection to models module.

--- varnishapp/manager.py
+++ varnishapp/manager.py
@@ -1,7 +1,17 @@
 from atexit import register
 from varnish import VarnishManager
+
 from .settings import MANAGEMENT_ADDRS, SECRET
 
 
-manager = VarnishManager(MANAGEMENT_ADDRS, SECRET)
+class DjangoVarnishManager(VarnishManager):
+    """
+    A subclass that sends the secret on every run.
+    """
+    def run(self, *commands, **kwargs):
+        kwargs.setdefault('secret', SECRET)
+        return super(DjangoVarnishManager, self).run(*commands, **kwargs)
+
+
+manager = DjangoVarnishManager(MANAGEMENT_ADDRS)
 register(manager.close)
